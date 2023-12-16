751ba3b31e5b6a4ce8d874362110cfbd39135312
MucahitKucuktepe <mehmet20132204@gmail.com>
2023-12-16 23:50:53 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,174 @@
+import React, { useEffect } from "react";
+import mailSvg from "./assets/mail.svg";
+import manSvg from "./assets/man.svg";
+import womanSvg from "./assets/woman.svg";
+import manAgeSvg from "./assets/growing-up-man.svg";
+import womanAgeSvg from "./assets/growing-up-woman.svg";
+import mapSvg from "./assets/map.svg";
+import phoneSvg from "./assets/phone.svg";
+import padlockSvg from "./assets/padlock.svg";
+import cwSvg from "./assets/cw.svg";
+import Footer from "./components/footer/Footer";
+import axios from "axios";
+import { useState } from "react";
+
+const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
+
+function App() {
+  const [user, setUser] = useState({
+    gender: "",
+    name: "",
+    location: "",
+    email: "",
+    dob: "",
+    phone: "",
+    login: "",
+    picture: "",
+  });
+
+  const [img, setImg] = useState(true);
+  const [item, setItem] = useState("name");
+  const [info, setInfo] = useState("");
+  const [sign, setSign] = useState(true);
+
+  const getUser = () => {
+    const url = "https://randomuser.me/api/";
+    const res = axios(url)
+      .then((res) => setUser(res.data.results[0]))
+      .catch((error) => console.log(console.error()));
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  const {
+    gender,
+    name,
+    location:{city},
+    email,
+    dob:{age},
+    phone,
+    login,
+    picture: { medium },
+  } = user;
+
+  console.log(email);
+  console.log(gender);
+  console.log(user);
+  const handleName = () => {
+    setItem("name");
+    setInfo(name);
+    setSign(true);
+  };
+
+  const handleMail = () => {
+    setItem("email");
+    setInfo(email);
+    setSign(false);
+  };
+  const handleAge=()=>{
+        setItem("Age");
+        setInfo(age);
+        setSign(false);
+  }
+  
+  const handleLocation=()=>{
+     setItem("Location");
+     setInfo(city);
+     setSign(false);
+  }
+  return (
+    <main>
+      <div className="block bcg-orange">
+        <img src={cwSvg} alt="cw" id="cw" />
+      </div>
+      <div className="block">
+        <div className="container">
+          <img src={medium} alt="random user" className="user-img" />
+          <p className="user-title">My {item} is</p>
+          <p className="user-value">
+            {sign ? (
+              <span>
+                {name?.first} {" "}
+                {name?.last}
+              </span>
+            ) : (
+              <span>{info}</span>
+            )}
+          </p>
+          <div className="values-list">
+            <button
+              className="icon"
+              data-label="name"
+              type="button"
+              onClick={handleName}
+            >
+              {gender === "female" ? (
+                <img src={womanSvg} alt="user" id="iconImg" />
+              ) : (
+                <img src={manSvg} alt="user" id="iconImg" />
+              )}
+            </button>
+            <button
+              className="icon"
+              data-label="email"
+              type="button"
+              onClick={handleMail}
+            >
+              <img src={mailSvg} alt="mail" id="iconImg" />
+            </button>
+            <button
+              className="icon"
+              data-label="age"
+              type="button"
+              onClick={handleAge}
+            >
+              {gender === "female" ? (
+                <img src={womanAgeSvg} alt="user" id="iconImg" />
+              ) : (
+                <img src={manAgeSvg} alt="user" id="iconImg" />
+              )}
+            </button>
+            <button className="icon" data-label="street" type="button" onClick={handleLocation}>
+              <img src={mapSvg} alt="map" id="iconImg" />
+            </button>
+            <button className="icon" data-label="phone">
+              <img src={phoneSvg} alt="phone" id="iconImg" />
+            </button>
+            <button className="icon" data-label="password">
+              <img src={padlockSvg} alt="lock" id="iconImg" />
+            </button>
+          </div>
+          <div className="btn-group">
+            <button className="btn" type="button" onClick={getUser}>
+              new user
+            </button>
+            <button className="btn" type="button">
+              add user
+            </button>
+          </div>
+
+          <table className="table">
+            <thead>
+              <tr className="head-tr">
+                <th className="th">Firstname</th>
+                <th className="th">Email</th>
+                <th className="th">Phone</th>
+                <th className="th">Age</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="body-tr"></tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Footer />
+      </div>
+    </main>
+  );
+}
+
+export default App;
